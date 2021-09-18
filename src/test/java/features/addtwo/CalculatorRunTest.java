17d3f621232ce4d020c137d0e764b4c88260bb60
Jane Jin(Bingyu) <jbyjane@qq.com>
2021-09-18 13:58:42 +0800

    tech: add junit report to cucumber plugin
    (cherry picked from commit 71c7d6f510aa944573aee4b4e0eca45a7cc25b7a)

--- src/test/java/features/addtwo/CalculatorRunTest.java
+++ src/test/java/features/addtwo/CalculatorRunTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features="src/test/resources/features/addtwo/calculator.feature",
         plugin = {
+                "junit:target/junit-cucumber-reports/calculatortwo.xml",
                 "json:target/cucumber-reports/calculatortwo.json",
                 "pretty"
         },
